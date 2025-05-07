86222da3520095957b906dfabbbfbcb3a6632031
Danil Danonovich <danillpp2000@gmail.com>
2025-05-08 01:11:37 +0300

    Home work 5.1 done i make simple TaxService

---
+++ src/com/gmail/ahmedded/homework/homework5/TaxService.java
@@ -0,0 +1,23 @@
+package com.gmail.ahmedded.homework.homework5;
+
+import java.util.Scanner;
+
+public class TaxService {
+    public static void main(String[] args){
+        Scanner ProfitSkaner = new Scanner(System.in);
+        System.out.println("Type your profit : ");
+        double income = ProfitSkaner.nextDouble();
+
+        double tax;
+
+        if ( income <= 10000 ){
+            tax = income * 0.025;
+        }else if ( income > 10000 && income <= 25000 ){
+            tax = income * 0.043;
+        }else{
+            tax = income * 0.06;
+        }
+
+        System.out.printf("Your end bill with tax was : " + tax);
+    }
+}
