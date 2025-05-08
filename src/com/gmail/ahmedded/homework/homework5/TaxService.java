1b16c264cbfca4f93a17ec162c17cbde9bf8876f
Danil Danonovich <danillpp2000@gmail.com>
2025-05-08 14:48:13 +0300

    Home work 5.1 If applied, this commit will done i make simple TaxService

--- src/com/gmail/ahmedded/homework/homework5/TaxService.java
+++ src/com/gmail/ahmedded/homework/homework5/TaxService.java
@@ -3,21 +3,21 @@ package com.gmail.ahmedded.homework.homework5;
 import java.util.Scanner;
 
 public class TaxService {
-    public static void main(String[] args){
-        Scanner ProfitSkaner = new Scanner(System.in);
-        System.out.println("Type your profit : ");
-        double income = ProfitSkaner.nextDouble();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type your profit: ");
 
+        double income = scanner.nextDouble();
         double tax;
 
-        if ( income <= 10000 ){
+        if (income <= 10000) {
             tax = income * 0.025;
-        }else if ( income > 10000 && income <= 25000 ){
+        } else if (income <= 25000) {
             tax = income * 0.043;
-        }else{
+        } else {
             tax = income * 0.06;
         }
 
-        System.out.printf("Your end bill with tax was : " + tax);
+        System.out.printf("Your end bill with tax is: ", tax);
     }
 }
