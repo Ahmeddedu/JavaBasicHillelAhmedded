3cc47b25fc380aed1071b565c7c7773657f3b698
Danil Danonovich <danillpp2000@gmail.com>
2025-05-08 15:23:20 +0300

    Home work 5.1 Fix bug in println out

--- src/com/gmail/ahmedded/homework/homework5/TaxService.java
+++ src/com/gmail/ahmedded/homework/homework5/TaxService.java
@@ -6,18 +6,18 @@ public class TaxService {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Type your profit: ");
-
         double income = scanner.nextDouble();
+
         double tax;
 
-        if (income <= 10000) {
+        if ( income <= 10000 ){
             tax = income * 0.025;
-        } else if (income <= 25000) {
+        }else if ( income > 10000 && income <= 25000 ){
             tax = income * 0.043;
-        } else {
+        }else{
             tax = income * 0.06;
         }
 
-        System.out.printf("Your end bill with tax is: ", tax);
+        System.out.println("Your end bill with tax is: " + tax);
     }
 }
