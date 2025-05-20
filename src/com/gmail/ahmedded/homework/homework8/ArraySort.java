8e701809e0f1170e256efb29b29750faa3f3b8da
Danil Danonovich <danillpp2000@gmail.com>
2025-05-20 22:22:43 +0300

    Home work 7.1 Rename peremenni :) and fix some kode

--- src/com/gmail/ahmedded/homework/homework8/ArraySort.java
+++ src/com/gmail/ahmedded/homework/homework8/ArraySort.java
@@ -9,23 +9,23 @@ public class ArraySort {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-
         System.out.print("Whitch size array do you want ? Type only number : ");
-        int sizeArrayScaner = scanner.nextInt();
-        if (sizeArrayScaner > 0) {
-            sizeArrayScaner = sizeArrayScaner;
+        int size = scanner.nextInt();
+        if (size > 0) {
+            size = size;
         } else {
             System.out.println("You type number under 0 type only number upper zero restart program");
+            return;
         }
 
         System.out.print("What number will the random start in the array : ");
-        int firstRandomNumber = scanner.nextInt();
+        int minValue = scanner.nextInt();
         System.out.print("What number will the random end in the array : ");
-        int endRandomNumber = scanner.nextInt();
+        int maxValue = scanner.nextInt();
 
-        int[] array = new int[sizeArrayScaner];
+        int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(firstRandomNumber, endRandomNumber);
+            array[i] = random.nextInt(minValue, maxValue);
         }
 
         System.out.println(" ");
@@ -43,6 +43,7 @@ public class ArraySort {
             }
             array[j + 1] = curren;
         }
+
         System.out.println(" ");
         System.out.println("Your sort array : ");
         for (int i = 0; i < array.length; i++) {
@@ -50,29 +51,29 @@ public class ArraySort {
         }
 
         System.out.print("What variant of sort array do you want type 1) to Liner search or 2) Binar search : ");
-        int variantSearch = scanner.nextInt();
-        if (variantSearch == 1) {
+        int searchOption = scanner.nextInt();
+        if (searchOption == 1) {
             System.out.print("What number you want to find in array : ");
-            int numberArrayFound = scanner.nextInt();
+            int target = scanner.nextInt();
 
             for (int i = 0; i < array.length; i++) {
-                if (numberArrayFound == array[i]) {
-                    System.out.println("element " + i + ") - " + numberArrayFound);
+                if (target == array[i]) {
+                    System.out.println("element " + i + ") - " + target);
                 }
             }
-        } else if (variantSearch == 2) {
+        } else if (searchOption == 2) {
             System.out.print("What number you want to find in array : ");
-            int numberArrayFound = scanner.nextInt();
+            int target = scanner.nextInt();
 
             int left = 0;
             int right = array.length - 1;
 
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-                if (array[mid] == numberArrayFound) {
-                    System.out.println("element " + mid + ") found your namber at index : " + numberArrayFound);
+                if (array[mid] == target) {
+                    System.out.println("element " + mid + ") found your namber at index : " + target);
                     break;
-                } else if (array[mid] < numberArrayFound) {
+                } else if (array[mid] < target) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
@@ -81,7 +82,5 @@ public class ArraySort {
         } else {
             System.out.println("You type number not 1 and 2 restart program");
         }
-
-
     }
 }
