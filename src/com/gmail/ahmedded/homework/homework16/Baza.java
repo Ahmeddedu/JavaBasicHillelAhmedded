20e689e1f3dff310ac8b066175a128c52720f9be
Ahmeddedu <156914766+Ahmeddedu@users.noreply.github.com>
2025-06-22 02:17:50 +0300

    Home work 16 my local start

---
+++ src/com/gmail/ahmedded/homework/homework16/Baza.java
@@ -0,0 +1,19 @@
+package com.gmail.ahmedded.homework.homework16;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Baza {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Which color do you want back(1) or red(2) or white(3) ");
+        int color = scanner.nextInt();
+
+        System.out.print("Which number do you want ?");
+        int number = scanner.nextInt();
+
+        
+    }
+}
